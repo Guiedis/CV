34e28233d98de3d774e3327431fae770adba776c
Guido Fernández <50142407+Guiedis@users.noreply.github.com>
2022-05-28 18:30:46 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,45 @@
 ///////////////////////////////////////////////////////////
+// Make Mobile navigation works
+///////////////////////////////////////////////////////////
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth Scrolling Animation
+///////////////////////////////////////////////////////////
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scrolls back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scrolls to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
 // Sticky navigation
 ///////////////////////////////////////////////////////////
 
